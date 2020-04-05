dfc0c78d9f0137f9f5e9aaeede4e653ace28e96a
julius <microsoftjulius>
2020-04-05 18:13:10 -0400

    Added a parents relation to the students model

--- app/UsersPackage/Students.php
+++ app/UsersPackage/Students.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Students extends Model
 {
     protected $fillable = ['first_name','last_name','date_of_birth','image_path','guardian_id','class_id','status'];
+
+    public function studentParent(){
+        return $this->belongsTo('App\UsersPackage\ParentsModel');
+    }
 }
