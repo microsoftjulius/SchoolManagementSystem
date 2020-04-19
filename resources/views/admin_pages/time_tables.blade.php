70e10fb5f812e0e3fa1e5f8bcd9e7a417f0038eb
julius <microsoftjulius>
2020-04-19 13:15:30 -0400

    Added cards on time table

--- resources/views/admin_pages/time_tables.blade.php
+++ resources/views/admin_pages/time_tables.blade.php
@@ -12,7 +12,7 @@
                         </div>
                     </div><br>
                     <div class="container-fluid mt-n10">
-                        {{-- @include('admin_layouts.cards') --}}
+                        @include('admin_layouts.cards')
                         @include('admin_layouts.data_tables')
                     </div>
                 </main>
